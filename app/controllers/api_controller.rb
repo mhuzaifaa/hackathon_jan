198b34c3bbd09a33af425645bc6c0d69905fb377
Huzaifa Shakir <huzaifa.shakir@storyful.com>
2024-01-24 13:39:02 +0000

    Added OpenAi Server and endpoint

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,29 @@
+# app/controllers/api_controller.rb
+
+require 'rest-client'
+require_relative '../services/openai_service'
+
+class ApiController < ApplicationController
+  def get_recommendation
+    user_data = params[:user_data] # You can adjust this based on your FE request format
+
+    # Generate a prompt using user data
+    prompt = build_prompt(user_data)
+
+    # Make a request to OpenAI
+    response = send_to_openai(prompt)
+    Rails.logger.info "response: #{response}" 
+
+      render json: { recommendation: response['choices'][0]['message'] }
+  end
+
+  private
+
+  def build_prompt(user_data)
+    "Generate 5 recommended stories based on the user's downloaded history and the similarity between story keywords for the user's downloaded story (i.e., #{user_data}). Output should be in the following JSON format: { 'story_title': 'story_link', ... }." 
+  end
+
+  def send_to_openai(prompt)
+    OpenaiService.new.generate_response(prompt)
+  end
+end
---
+++ app/services/openai_service.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'openai'
+require 'csv'
+
+class OpenaiService
+  attr_reader :client
+
+  def initialize
+    @client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+  end
+
+  def generate_response(user_prompt)
+    messages = [
+      { role: 'system', content: "Providing user downloaded story titles history. Headers are Download Date, Story Title. #{user_download_data}" },
+      { role: 'system', content: "Providing story data. Headers are Publish Date, Story Title, Keywords. #{story_recommendation_data}" },
+      { role: 'user', content: user_prompt }
+    ]
+  
+    Rails.logger.info "Messages: #{messages}" 
+
+    # raise messages.inspect
+    response = client.chat(
+      parameters: {
+        model: 'gpt-3.5-turbo',
+        messages: messages
+      }
+    )
+
+    response
+  end
+
+  private
+
+  def read_csv_file
+    csv_file_path = Rails.root.join('public', 'user.csv')
+    CSV.read(csv_file_path, headers: true)
+  end
+
+  def user_download_data
+    read_csv_file.map do |row|
+      [row['Download Date'], row['Story Title']].to_s
+    end.take(10)
+  end
+
+  def story_recommendation_data
+    read_csv_file.map do |row|
+      row.to_s
+    end.take(50)
+  end
+end
